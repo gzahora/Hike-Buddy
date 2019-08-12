c4b5db939e38745261c50583be7da61981c5a241
Greg Zahora <gregzahora3@gmail.com>
2019-08-12 11:18:26 -0400

    setting up heroku connection

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: "root",
+    password: process.env.PASSWORD,
+    database: "nationalParks",
+    host: "localhost",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "testdb",
+    host: "localhost",
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+};
