3514085026bd87347bb5f2d0bfd320f8b1f70693
Greg Zahora <gregzahora3@gmail.com>
2019-08-13 17:15:22 -0400

    added search functions

--- models/parks.js
+++ models/parks.js
@@ -13,11 +13,11 @@ module.exports = function(sequelize, Sequelize) {
       allowNull: false
     },
     latitude: {
-      type: Sequelize.INTEGER,
+      type: Sequelize.DECIMAL(8, 4),
       allowNull: false
     },
     longitude: {
-      type: Sequelize.INTEGER,
+      type: Sequelize.DECIMAL(8, 4),
       allowNull: false
     },
     ranking: {
@@ -33,6 +33,22 @@ module.exports = function(sequelize, Sequelize) {
       type: Sequelize.BOOLEAN,
       allowNull: false,
       defaultValue: false
+    },
+    image: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    },
+    url: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    map: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    },
+    short_name: {
+      type: Sequelize.STRING,
+      allowNull: false
     }
   });
   return Park;
