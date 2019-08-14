5355fe314a79d2afea11984d01f785bf4fe16d5d
Saakshig12 <saakshig12@gmail.com>
2019-08-14 11:29:16 -0400

    updates

--- nodelogin/login.js
+++ nodelogin/login.js
@@ -50,7 +50,7 @@ app.post("/auth", function(request, response) {
   }
 });
 
-app.get("/home", function(request, response) {
+app.get("/profile", function(request, response) {
   if (request.session.loggedin) {
     response.send("Welcome back, " + request.session.username + "!");
   } else {
